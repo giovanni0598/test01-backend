1b13177325613c935bb24ecb0911e7b879b0b19f
Giovanni Morais Reyna <gmreyna@stefanini.com>
2024-06-20 01:22:53 -0500

    feat(swagger): se agrego swagger con uso de token en el mismo para aplicar como postman

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Post, Body, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
+    @ApiTags('Auth')
     @Post('/login')
     async login(@Body() body) {
         const token = await this.authService.generateToken(body);
--- src/exchange/exchange.controller.ts
+++ src/exchange/exchange.controller.ts
@@ -1,11 +1,16 @@
 import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { ExchangeService } from './exchange.service';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
+
+
 @Controller('exchange')
 export class ExchangeController {
 
     constructor(private readonly exchangeService: ExchangeService) { }
 
+    @ApiBearerAuth()
+    @ApiTags('Exchange')
     @UseGuards(JwtAuthGuard)
     @Get('')
     getExchanges(): string {
@@ -13,9 +18,23 @@ export class ExchangeController {
         return listExchanges
     }
 
+    @ApiBearerAuth()
+    @ApiTags('Exchange')
+    @ApiBody({
+        schema: {
+            type: 'object',
+            properties: {
+                rate: {
+                    type: 'number',
+                    description: 'Tasa de cambio a actualizar.'
+                }
+            },
+            required: ['rate']
+        }
+    })
     @UseGuards(JwtAuthGuard)
     @Put(':currency')
-    updateExchangeRate(@Param('currency') currency: string, @Body() body: { rate: number }) {
+    updateExchangeRate(@Param('currency') currency: string, @Body('rate') body: { rate: number }) {
         const { rate } = body;
         this.exchangeService.updateExchangeRate(currency, rate);
         return { message: 'La conversion a sido modificado correctamente, consulta la ruta /exchange para confirmarlo' };
